2d27d3bd00895f83b8c4b515104e9283b7ced8a0
Ankit Kumar <ankitiit263@gmail.com>
2025-04-01 18:42:56 +0530

    Update productCategory.js

--- server/constants/productCategory.js
+++ server/constants/productCategory.js
@@ -1,17 +1,3 @@
-// const category = [
-//   "rice",
-//   "wheat",
-//   "nuts",
-//   "sugar",
-//   "spices",
-//   "fruits",
-//   "vegetables",
-//   "pulses",
-// ];
-
-// module.exports = category;
-
-
  const category = [
   "rice",
   "wheat",
