635a20fbeaf30a5a127996501ce9824500996529
Ankit Kumar <ankitiit263@gmail.com>
2025-04-01 18:44:02 +0530

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,39 +1,6 @@
 const Order = require("../models/orderSchema");
 const { decreaseProductStocks } = require("../services/productServices");
 
-// Add Order
-// const addOrder = async (req, res) => {
-//   try {
-//     const orders = req.body;
-//     const userId = req.userId;
-//     console.log("Received User ID in Controller:", userId);
-//     // Check orders variable is an array
-//     if (!userId) {
-//       return res.status(401).json({ message: "Unauthorized: User ID is missing" });
-//     }
-//     if (!Array.isArray(orders)) {
-//       return res.status(400).send({ message: "Invalid orders data" });
-//     }
-
-//     for (const order of orders) {
-//       order.userId = userId;
-
-//       let data = Order(order);
-//       let result = await data.save();
-//       console.log(result);
-//       await decreaseProductStocks(data.productId, data.orderQty);
-//     }
-
-//     res.status(200).send({ message: `All orders successfully received` });
-//   } catch (error) {
-//     res.status(500).send("Something went wrong!");
-//     console.log(error);
-//   }
-// };
-
-
-
-
 const addOrder = async (req, res) => {
   try {
     const orders = req.body;
